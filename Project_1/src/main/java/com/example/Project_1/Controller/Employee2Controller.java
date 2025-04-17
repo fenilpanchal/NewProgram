25d909cad32c80e69678467a96a8d81958e4364e
fenilpanchal <105039993+fenilpanchal@users.noreply.github.com>
2025-04-17 17:29:20 +0530

    Update Employee2Controller.java
    map-struct

--- Project_1/src/main/java/com/example/Project_1/Controller/Employee2Controller.java
+++ Project_1/src/main/java/com/example/Project_1/Controller/Employee2Controller.java
@@ -4,15 +4,18 @@ import com.example.Project_1.Model.Employee2;
 import com.example.Project_1.Dto.EmployeeDto;
 import com.example.Project_1.Services.Employee2Services;
 import com.example.Project_1.Services.JWTServices;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/Employee")
 public class Employee2Controller {
@@ -23,28 +26,47 @@ public class Employee2Controller {
     @Autowired
     private JWTServices jwtServices;
 
+///Dto
 
     @GetMapping("/get")
-    public List<EmployeeDto> getAll(){
-        return employee2Services.getAll();
+    public ResponseEntity<List<EmployeeDto>> getAll(){
+        return ResponseEntity.ok(employee2Services.getAll());
     }
 
-    @PostMapping("/add")
-    public EmployeeDto create(@RequestBody EmployeeDto employeeDto){
-        return employee2Services.createEmployee(employeeDto);
+    @GetMapping("/getBy/{id}")
+    public ResponseEntity<EmployeeDto> getById(@PathVariable Integer id){
+        return ResponseEntity.ok(employee2Services.getById(id));
     }
 
+    @PostMapping
+    public ResponseEntity<EmployeeDto> create(@RequestBody EmployeeDto employeeDto){
+        return new ResponseEntity<>(employee2Services.createEmployee(employeeDto),HttpStatus.CREATED);
+    }
+//
+//    @PutMapping("/update/{id}")
+//    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Integer id,@RequestBody EmployeeDto employeeDto){
+//        return ResponseEntity.ok(employee2Services.updateEmployee(id, employeeDto));
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public ResponseEntity<Void> deleteEmployee(@PathVariable Integer id){
+//        employee2Services.deleteEmployee(id);
+//        return ResponseEntity.noContent().build();
+//    }
+
+///
     @GetMapping
     public List<Employee2> Get(){
         return employee2Services.GetAll();
     }
 
     @GetMapping("/{id}")
-    public Employee2 GetByIdD(@PathVariable Integer id){
-        return employee2Services.GetById(id).orElseThrow();
+    public ResponseEntity<Employee2> GetByIdD(@PathVariable Integer id){
+         Employee2 employee2 = employee2Services.GetById(id);
+         return ResponseEntity.ok(employee2);
     }
 
-   @PostMapping("/register")
+    @PostMapping("/register")
     @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
     public ResponseEntity<Employee2> register(
             @RequestBody Employee2 employee,
@@ -54,7 +76,7 @@ public class Employee2Controller {
         return ResponseEntity.ok(employee2Services.register(employee, role, authentication));
     }
 
-   @PutMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Employee2> Update(@PathVariable Integer id,
                                     @RequestBody Employee2 employee2,
                                     @RequestParam(required = false, defaultValue = "USER") String role,
@@ -62,7 +84,8 @@ public class Employee2Controller {
         log.info("Update API called by.: {}", authentication.getName());
         return ResponseEntity.ok(employee2Services.UpdateE(id,employee2, role, authentication));
     }
-  @DeleteMapping("/{id}")
+
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id,@RequestHeader("Authorization") String token){
         if (token == null || !token.startsWith("Bearer ")) {
             log.warn("Unauthorized access attempt!!");
@@ -72,15 +95,18 @@ public class Employee2Controller {
         List<String> roles = jwtServices.extraRole(token);
         log.info("Extracted roles from token: {}", roles);
 
-        if (!roles.contains("SUPER_ADMIN")) {
-            log.warn("Access Denied: Only Super-Admin can delete Admin and users..");
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access Denied: Only Super-Admin can delete Admin and Users..");
+        if (roles.contains("SUPER_ADMIN")) {
+            employee2Services.Delete(id, "SUPER_ADMIN");
+            return ResponseEntity.noContent().build();
+        } else if (roles.contains("ADMIN")) {
+            employee2Services.Delete(id, "ADMIN");
+            return ResponseEntity.noContent().build();
         }
-        employee2Services.Delete(id,"SUPER_ADMIN");
-        return ResponseEntity.noContent().build();
-    }
 
-/// Search
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body("Access Denied: Only Super-Admin can delete Admins, and Admins can delete only Users.");
+    }
+    /// Search
     @GetMapping("/search")
     public ResponseEntity<List<Employee2>>Search(@RequestParam String keyword){
         if (keyword.length()< 2){
@@ -97,7 +123,7 @@ public class Employee2Controller {
 
     @PostMapping("/logout/{id}")
     public ResponseEntity<String> logout(@PathVariable Integer id,@RequestHeader("Authorization") String token){
-       Optional<Employee2> emp = employee2Services.GetById(id);
+       Optional<Employee2> emp = Optional.ofNullable(employee2Services.GetById(id));
 
         if (emp.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
@@ -111,5 +137,4 @@ public class Employee2Controller {
         }
         return ResponseEntity.ok( "Log-out SuccessFully .. ");
     }
-
 }
